b7f2fa49eb592b36634e73ecc4aa8e6a31685e5d
Karan <karanmahatocse@gmail.com>
2024-03-06 20:12:33 +0530

    new changes

--- Chat-app/Server/index.js
+++ Chat-app/Server/index.js
@@ -5,6 +5,8 @@ const socketIO = require("socket.io");
 
 const app = express();
 
+const users=[{}]
+
 app.use(cors());
 const port = 4000 || process.env.PORT;
 const server = http.createServer(app);
@@ -18,6 +20,21 @@ app.get('/', (req, res) =>{
 
 io.on('connection',(socket)=>{
     console.log(`a user connected to ${socket.id}`);
+   
+    socket.on('joined',({user})=>{
+        users[socket.id] = user
+        console.log(`${user} is now connected`)
+        socket.emit("welcome", {user:"Admin",message:`welcome to the chat,${users[socket.id]}`})
+        socket.broadcast.emit("userjoined",{user:"Admin",message:`${users[socket.id]} has now joined`})
+    
+    })
+
+    socket.on('disconnect',()=>{
+        console.log(`${users[socket.id]} has disconnected`)
+        socket.broadcast.emit("userleft",{user:"Admin",message:`${users[socket.id]} has left`})
+        delete users[socket.id]
+    })
+
 })
 
 
